fde3aedca4956db7a4c2ee1850069bc0021c3151
Gustavo Marttos <marttosc@gmail.com>
2016-04-10 23:55:19 -0300

    Unused imports were removed.

--- Wallet/Infra/DAOUser.cs
+++ Wallet/Infra/DAOUser.cs
@@ -3,9 +3,6 @@ using Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infra
 {
